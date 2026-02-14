25a4cd7a255fc9c42706c776f8156d96b75248c3
Nipun Taneja <ntaneja@sfsu.edu>
2026-02-14 13:55:52 -0800

    ui changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
         fields={dashFields}
         onFieldsChange={setDashFields}
         onBack={handleDashboardBack}
-        onEditReanalyze={handleDashboardBack}
       />
     );
   }
